dd34be87d5e6555b7f68ca8be184950aeea7551c
mrb <git@mrb.zone>
2024-08-14 21:20:57 -0300

    Add regular expression support, and make some other architectural changes. In particular, using serde for config toml parsing.

--- sortmail/src/main.rs
+++ sortmail/src/main.rs
@@ -4,9 +4,14 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 
 use maildir::Maildir;
-use toml::Table;
 use anyhow::{Context, Result};
 use clap::{Parser};
+use regex::Regex;
+use serde::{Deserialize, Deserializer};
+
+//
+// Command-line args
+//
 
 /// Read email message from stdin and deliver it to the correct
 /// Maildir based on the supplied filtering config.
@@ -21,6 +26,10 @@ struct Args {
     #[arg(short = 'n', long = "dry-run")]
     dry_run: bool,
 
+    /// Print out the address map before performing delivery
+    #[arg(short = 'P', long = "print-address-map")]
+    print_address_map: bool,
+
     /// Use an alternate root Maildir (default: $HOME/Maildir)
     #[arg(short = 'M', long = "maildir", value_name = "/path/to/Maildir")]
     override_root_maildir: Option<PathBuf>,
@@ -30,67 +39,123 @@ struct Args {
     original_recipient_environment_variable: Option<String>
 }
 
+//
+// Config file
+//
 
-/// Load config_file as a TOML file containing a mapping of email
-/// addresses to Maildir mailboxes.
-///
-/// Input file should contain tables with a single `addresses` key
-/// containing newline-separated email addresses, like:
-///
-/// [MailboxName]
-/// addresses = """
-/// address1@example.com
-/// address2@example.com
-/// """
-///
-/// Return the mapping of each email address to the Maildir mailbox
-/// name it should be sorted into.
-fn load_mappings(config_file: &Path) -> Result<HashMap<String, String>> {
-    let contents = std::fs::read_to_string(config_file)
-        .with_context(|| format!("Error opening config file {}", config_file.display()))?;
+type ConfigToml = HashMap<String, ConfigMailbox>;
 
-    let document: Table = toml::from_str(&contents)
-        .with_context(|| format!("Error parsing config file {}", config_file.display()))?;
+#[derive(Deserialize, Debug)]
+struct ConfigMailbox {
+    #[serde(default, deserialize_with = "deserialize_email_addresses_separated_by_newlines")]
+    addresses: Vec<String>,
 
-    let addresses_to_mailboxes: HashMap<_, _> = document
-        .iter()
-        .filter_map(
-            |(mailbox_name, mailbox_options_value)| {
-                let toml::Value::Table(mailbox_options) = mailbox_options_value else {
-                    // Skip configuration (any key-value pairs not in a table)
-                    return None;
-                };
-
-                let addresses_toml = &mailbox_options["addresses"];
-
-                let toml::Value::String(addresses) = addresses_toml else {
-                    return Some(Err(Box::new(std::io::Error::from(std::io::ErrorKind::InvalidData)))
-                                .with_context(||
-                                    format!(
-                                        "Expected a string value for addresses in config file {} but found {:?}",
-                                        config_file.display(),
-                                        addresses_toml
-                                    )
-                                )
-                    );
-                };
-
-                let address_to_mailbox: Vec<(String, String)> = addresses.split("\n")
-                    .map(|addr| addr.trim())
-                    .filter(|addr| !addr.is_empty())
-                    .map(|addr| (String::from(addr.to_lowercase()), String::from(mailbox_name)))
-                    .collect();
-
-                Some(Ok(address_to_mailbox))
-            }
-        ).collect::<Result<Vec<_>, _>>()?
-        .into_iter()
-        .flatten()
-        .collect();
-
-    Ok(addresses_to_mailboxes)
+    #[serde(default, deserialize_with = "deserialize_email_addresses_separated_by_newlines")]
+    re_addresses: Vec<String>
 }
 
+fn deserialize_email_addresses_separated_by_newlines<'de, D: Deserializer<'de>>(d: D) -> Result<Vec<String>, D::Error> {
+    let s = String::deserialize(d)?;
+
+    Ok(s.split("\n")
+       .map(|addr| addr.trim().to_lowercase())
+       .filter(|addr| !addr.is_empty())
+       .collect())
+}
+
+//
+// Address map
+//
+
+#[derive(Debug)]
+struct AddressMap {
+    exact_address_to_mailbox_name: HashMap<String, String>,
+    address_regex_to_mailbox_name: Vec<(Regex, String)>
+}
+
+impl AddressMap {
+    /// Load config_file as a TOML file containing a mapping of email
+    /// addresses to Maildir mailboxes.
+    ///
+    /// Input file should contain tables with a single `addresses` key
+    /// containing newline-separated email addresses, like:
+    ///
+    /// [MailboxName]
+    /// addresses = """
+    /// address1@example.com
+    /// address2@example.com
+    /// """
+    /// re_addresses = """
+    /// ^local_part@
+    /// @things.example.com$
+    /// """
+    ///
+    /// Return the mapping of each email address to the Maildir mailbox
+    /// name it should be sorted into.
+    fn from_file(config_file: &Path) -> Result<AddressMap> {
+        let contents = std::fs::read_to_string(config_file)
+            .with_context(|| format!("Error opening config file {}", config_file.display()))?;
+
+        let config: ConfigToml = toml::from_str(&contents)
+            .with_context(|| format!("Error parsing config file {}", config_file.display()))?;
+
+
+        let addresses_to_mailboxes: HashMap<_, _> = config
+            .iter()
+            .flat_map(
+                |(mailbox_name, config_mailbox)| {
+                    let addresses = &config_mailbox.addresses;
+
+                    let address_and_mailbox: Vec<(String, String)> = addresses
+                        .iter()
+                        .map(|addr| (String::from(addr), String::from(mailbox_name)))
+                        .collect();
+
+                    address_and_mailbox
+                }
+            )
+            .collect();
+
+        let re_addresses_to_mailboxes: Result<Vec<(_, _)>, _> = config
+            .iter()
+            .flat_map(
+                |(mailbox_name, config_mailbox)| {
+                    let address_regexes = &config_mailbox.re_addresses;
+
+                    let address_regexes_and_mailbox: Vec<_> = address_regexes
+                        .iter()
+                        .map(|addr_re| Regex::new(addr_re).
+                             with_context(|| format!("Error parsing regular expression {addr_re}")).
+                             map(|re| (re, String::from(mailbox_name))))
+                        .collect();
+
+                    address_regexes_and_mailbox
+                }
+            )
+            .collect();
+
+        Ok(AddressMap {
+            exact_address_to_mailbox_name: addresses_to_mailboxes,
+            address_regex_to_mailbox_name: re_addresses_to_mailboxes?
+        })
+    }
+
+    fn mailbox_name_for_address(&self, address: &str) -> Option<&str> {
+        if let Some(mailbox_name) = self.exact_address_to_mailbox_name.get(address) {
+            return Some(mailbox_name);
+        }
+
+        let matching_regex = self.address_regex_to_mailbox_name.iter().find(
+            |(ref re, _)| re.is_match(address)
+        );
+
+        matching_regex.map(|item| item.1.as_str())
+    }
+}
+
+//
+// Mailbox delivery
+//
 
 fn get_normalized_original_recipient_email_address(args: &Args) -> Result<String> {
     let env_variable: &str = match args.original_recipient_environment_variable {
@@ -120,22 +185,27 @@ fn sort_message_from_stdin(args: &Args) -> Result<()> {
         }
     };
 
-    let mappings = load_mappings(&args.config)
+    let mappings = AddressMap::from_file(&args.config)
         .with_context(|| format!("Error loading config file {}", args.config.display()))?;
+
+    if args.print_address_map {
+        dbg!(&mappings);
+    }
+
 
     // Save to maildir
 
     let original_recipient_email_address = get_normalized_original_recipient_email_address(args)?;
 
-    if let Some(mailbox_name) = mappings.get(&original_recipient_email_address) {
+    if let Some(mailbox_name) = mappings.mailbox_name_for_address(&original_recipient_email_address) {
         maildir.push(format!(".{mailbox_name}"));
     }
 
-    eprintln!(
+    println!(
         "Recipient {original_recipient_email_address}: Deliver to {}{}",
         maildir.display(),
         match args.dry_run {
-            true => " (dry run, no actual delivery will be made)",
+            true => " (dry run, no actual delivery will be performed)",
             false => ""
         }
     );
@@ -161,8 +231,7 @@ fn sort_message_from_stdin(args: &Args) -> Result<()> {
     Ok(())
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Args::parse();
-
-    sort_message_from_stdin(&args).unwrap();
+    sort_message_from_stdin(&args)
 }
